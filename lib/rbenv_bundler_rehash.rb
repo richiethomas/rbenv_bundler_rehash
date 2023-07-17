cc794a53c3ab0abb5766ac8e4cdc5bc65bfb56e4
Richie Thomas <rickthomas1980@gmail.com>
2023-07-17 12:44:42 -0400

    Try this again

--- lib/rbenv_bundler_rehash.rb
+++ lib/rbenv_bundler_rehash.rb
@@ -1,13 +1,15 @@
 require "rbenv_bundler_rehash/version"
 require 'open3'
+require 'byebug'
 
 module RbenvBundlerRehash
   class CouldNotRehashError < StandardError; end
 
   def self.perform
     stdout, stderr, status = Open3.capture3("rbenv rehash")
-    # unless stderr.empty?
-    #   raise CouldNotRehashError, stderr
-    # end
+    unless stderr.empty?
+      debugger
+      raise CouldNotRehashError, stderr
+    end
   end
 end
